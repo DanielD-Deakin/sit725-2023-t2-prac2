acb6d5080a45d923862a9111fcb59072c59fd247
Daniel D <s222311422@deakin.edu.au>
2023-07-20 18:28:18 +1000

    Added API GET request
    Added an API GET request for adding two (integer or float inputs) numbers together.

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var app = express();
-
-
-app.use(express.static(__dirname + '/'));
-
+var path = require("path");
 var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public"))); //setting root directory
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/fonts", express.static(path.join(__dirname, "fonts")));
+
+app.get('/addTwoNumber', (req, res) => {
+    // Allocating argument and converting to correct data type via parseFloat
+    let number_1 = parseFloat(req.query.number1);
+    let number_2 = parseFloat(req.query.number2);
+
+    // Performing the summation of the two nubmers
+    let num_sum = number_1 + number_2;
+
+    // Creating an object (dictionary) which holds the following properties
+    var obj = {statusCode: 200, message: 'success', data:num_sum};
+
+    // Returning the obj as a response
+    res.json(obj);
+});
+
 app.listen(port, () => {
     console.log("App listening to: " + port);
 });
